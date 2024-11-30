88b8db6b269aed2c075e70749eacb9b8edeac6cd
Amine Ben hammou <webneat@gmail.com>
2024-11-30 18:30:24 +0100

    Release 1.4.3

--- src/Err.ts
+++ src/Err.ts
@@ -1,7 +1,22 @@
 import type { ErrorDetails, ErrorKey } from './types/index.js'
 
 export class Err<K extends ErrorKey = 'Unknown'> extends Error {
-  constructor(public type: K, public details: ErrorDetails<K>) {
-    super(`${type}: ${JSON.stringify(details)}`)
+  constructor(
+    public type: K,
+    public details: ErrorDetails<K>,
+  ) {
+    super(`${type}: ${JSON.stringify(details, getCircularReplacer())}`)
+  }
+}
+
+function getCircularReplacer() {
+  const ancestors: unknown[] = []
+  return function (_: string, value: unknown) {
+    if (typeof value !== 'object' || value === null) return value
+    // @ts-ignore
+    while (ancestors.length > 0 && ancestors.at(-1) !== this) ancestors.pop()
+    if (ancestors.includes(value)) return '[Circular]'
+    ancestors.push(value)
+    return value
   }
 }
